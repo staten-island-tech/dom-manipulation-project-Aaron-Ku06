6c911576c0ff895e9bb62194054e0e67fbd5dbdc
Aaron-Ku06 <aaronku320@gmail.com>
2023-10-19 09:22:13 -0400

    Input Clearing and HTML

--- dom.js
+++ dom.js
@@ -1,15 +1,16 @@
 const DOMSelectors = {
     form: document.querySelector("#form"),
     firstName: document.querySelector(".first-name"),
-    h2s: document.querySelectorAll("h2"),
+    h3s: document.querySelectorAll("h3"),
 };
 
-console.log(DOMSelectors.h2s);
+console.log(DOMSelectors.h3s);
 
 DOMSelectors.form.addEventListener("submit", function(event){
 event.preventDefault();
 console.log(DOMSelectors.firstName.value);
-DOMSelectors.h2s.forEach(
+DOMSelectors.h3s.forEach(
     (el) => (el.textContent = DOMSelectors.firstName.value)
     );
+    document.getElementById("form").reset();
 });
